88a6775150ff19e5a8120abce6f11453e6958ba1
Lee <ltr0121@naver.com>
2020-08-23 22:01:11 +0900

    make blogproject app, set home url

---
+++ ebdjango/blogproject/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Blog
+
+# Create your views here.
+
+
+def home(request):
+    blogs = Blog.objects
+    return render(request, 'blogproject/home.html', {'blogs': blogs})
